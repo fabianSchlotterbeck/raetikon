e10f1a101dfb62138cf48709a3be97d9e010f540
Fabian Schlotterbeck <fabianschlotterbeck@MacBook-Pro-von-Fabian.local>
2023-08-12 19:50:13 +0200

    add data and analysis

---
+++ schesaplana/descr_and_infer_comp_scope.R
@@ -0,0 +1,164 @@
+library(ggplot2)
+library (lme4)
+library(lmerTest)
+library(Hmisc)
+library(blme)
+
+rm(list=ls())
+
+load ("preprocessed_data_schesaplana.RData")
+
+plots_dir<- file.path(getwd(),"plots")
+if(!dir.exists(plots_dir)) dir.create(plots_dir, recursive = TRUE, showWarnings = FALSE)
+
+#TODO: Maybe move to preprocessing
+items_comp_scope <- subset(items_comp_scope, roi == "rt.14")
+
+items_comp_scope$type <- as.factor(ifelse(items_comp_scope$conditions %in% 9:12, "exactly","less"))
+contrasts(items_comp_scope$type) <- rbind(-.5,.5)
+colnames(contrasts(items_comp_scope$type)) <- levels(items_comp_scope$type)[2]
+
+
+items_comp_scope$modal <- as.factor(ifelse(items_comp_scope$conditions %in% c(9,10,13,14), "present","absent"))
+contrasts(items_comp_scope$modal) <- rbind(-.5,.5)
+colnames(contrasts(items_comp_scope$modal)) <- levels(items_comp_scope$modal)[2]
+
+items_comp_scope$question <- as.factor(ifelse(items_comp_scope$conditions %in% c(9,11,13,15), "match","mismatch"))
+contrasts(items_comp_scope$question) <- rbind(-.5,.5)
+colnames(contrasts(items_comp_scope$question)) <- levels(items_comp_scope$question)[2]
+
+items_comp_scope$corr_resp  <- ifelse(items_comp_scope$conditions %in% c(9,11,13,15), 1, 0)
+items_comp_scope$acc <- items_comp_scope$corr_resp == items_comp_scope$response_num
+
+items_comp_scope$response_num <- ifelse(items_comp_scope$response == "True", 1, 0)
+
+ses<-aggregate(response_num~question+modal+type, data=items_comp_scope,
+               FUN=function(x) {binconf(x=sum(x), n=length(x))})
+
+agg<-cbind(cbind(ses[1:3],ses[,4][,1],ses[,4][,2],ses[,4][,3]))
+
+aggregate(1-acc~question+modal+type, data=items_comp_scope, FUN=mean)
+aggregate(1-acc~modal+type, data=items_comp_scope, FUN=mean)
+
+names(agg)<-c("question","modal","type", "mean_judgment", "lower", "upper")
+
+eb <- ggplot(data = agg, aes(question, mean_judgment, fill = modal)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  geom_errorbar(aes(ymin=lower, ymax=upper), width=.2,
+                position=position_dodge(.9))+
+  facet_wrap(~type)+
+  ylab("rel. freq. \"yes\" (and approx. 95% CIs)")+
+  theme(legend.title = element_text(size = 16),
+        legend.text = element_text(size = 16),
+        axis.title.x = element_text(size = 16),
+        axis.text.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        axis.text.y = element_text(size = 16),
+        strip.text = element_text(size=16))
+ggsave(file.path(plots_dir,"mean_judgments_comp_scope.pdf"), device="pdf", width=8, height=5)
+
+m.scope.0.0 <- glmer(response_num~question*modal*type
+                     +(1|item)+(1|id),
+                     family=binomial, data=items_comp_scope,
+                     glmerControl(optimizer = c("bobyqa")))
+
+#m.scope.0.all <- allFit(m.scope.0)
+
+#TODO: Recompute with effect coded contrast
+#adjust random effect structure
+m.scope.0.1 <- update(m.scope.0.0, .~.-(1|item))
+anova(m.scope.0.1,m.scope.0.0)
+
+m.scope.0.2 <- update(m.scope.0.0, .~.-(1|id))
+anova(m.scope.0.2,m.scope.0.0)
+
+m.scope.0.3 <- update(m.scope.0.1, .~.+(0+modal|id))
+anova(m.scope.0.3,m.scope.0.1)
+
+m.scope.0.4 <- update(m.scope.0.1, .~.+(0+type|id))
+#no convergence
+
+m.scope.0.5 <- update(m.scope.0.1, .~.+(0+question|id))
+anova(m.scope.0.5,m.scope.0.1)
+
+#simplify fixed effects
+m.scope.1.1 <- update(m.scope.0.1, .~.-question:modal:type)
+anova(m.scope.1.1,m.scope.0.1)
+summary(m.scope.0.1)
+
+items_comp_scope_exactly <- subset(items_comp_scope, type=="exactly")
+
+m.scope.exactly.0 <- glmer(response_num~question*modal
+                           #+ (0 + modal | id) + (0 + modal | item)
+                           #+ (1|item)
+                           + (1|id),
+                           family=binomial,
+                           data=items_comp_scope_exactly,
+                           glmerControl(optimizer = c("bobyqa")))
+
+
+m.scope.exactly.1 <- update(m.scope.exactly.0, .~.-question:modal)
+anova(m.scope.exactly.1, m.scope.exactly.0)
+summary(m.scope.exactly.0)
+
+items_comp_scope_exactly_mismatch <- subset(items_comp_scope_exactly, question=="mismatch")
+
+m.scope.exactly.mismatch.0 <- glmer(response_num~modal+(1|item)+(1|id),
+                                    family=binomial, data=items_comp_scope_exactly_mismatch,
+                                    glmerControl(optimizer = c("bobyqa")))
+summary(m.scope.exactly.mismatch.0)
+
+items_comp_scope_exactly_match <- subset(items_comp_scope_exactly, question=="match")
+
+m.scope.exactly.match.0 <- glmer(response_num~modal+(1|item)+(1|id),
+                                 family=binomial, data=items_comp_scope_exactly_match,
+                                 glmerControl(optimizer = c("bobyqa")))
+summary(m.scope.exactly.match.0)
+
+items_comp_scope_less <- subset(items_comp_scope, type=="less")
+
+m.scope.less.0 <- glmer(response_num~question*modal
+                        #+ (0 + modal | id) + (0 + modal | item)
+                        #+(1|item)
+                        +(1|id),
+                        family=binomial, data=items_comp_scope_less,
+                        glmerControl(optimizer = c("bobyqa")))
+
+m.scope.less.1 <- update(m.scope.less.0, .~.-question:modal)
+anova(m.scope.less.1, m.scope.less.0)
+summary(m.scope.less.0)
+
+m.scope.less.2 <- update(m.scope.less.1, .~.-modal)
+anova(m.scope.less.1, m.scope.less.2)
+summary(m.scope.less.2)
+
+
+#posthoc tests correctd alpha .125
+items_comp_scope_modal <- subset(items_comp_scope, modal=="present")
+
+m.scope.modal <- glmer(response_num~type*question
+                       #+(1|item)
+                       +(1|id),
+                       family=binomial, data=items_comp_scope_modal,
+                       glmerControl(optimizer = c("bobyqa")))
+summary(m.scope.modal)
+
+items_comp_scope_nomodal <- subset(items_comp_scope, modal=="absent")
+
+m.scope.nomodal <- glmer(response_num~type*question
+                         #+(1|item)
+                         +(1|id),
+                         family=binomial, data=items_comp_scope_nomodal,
+                         glmerControl(optimizer = c("bobyqa")))
+summary(m.scope.nomodal)
+
+
+##Bayesian regression weakly informative priors
+bm.scope.0.0 <- bglmer(response_num~question*modal*type
+                     +(1|item)+(1|id),
+                     family=binomial,
+                     data=items_comp_scope, 
+                     fixef.prior = normal(cov = diag(9,8)),
+                     glmerControl(optimizer = c("bobyqa")))
+
+summary(bm.scope.0.0)
